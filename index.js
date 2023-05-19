51f8f7d886bf256a1eb6cf02691e1963bb1b279b
jayakart <130072124+jayakart@users.noreply.github.com>
2023-05-19 18:05:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+const message = require("node-hello/index.js");
+message.sayHello()
